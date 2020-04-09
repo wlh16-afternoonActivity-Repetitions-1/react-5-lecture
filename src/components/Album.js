7ae398e823fc670774a9c6cb6dd70cf19d4d306d
Jacob Hall <halljaco@yahoo.com>
2020-04-09 13:45:50 -0600

    class lecture example React 5

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,15 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './style.css'
 
 const Album = ({ data }) => {
-  const { artist, title, year, image } = data
+  const { artist, title, year, image, id } = data
   return (
     <div className="album">
       <img alt={title} src={image} className="album-art" />
       <div className="album-info-hold">
-        <p>
-          {artist} - {title}
-        </p>
+        <Link to={`/album/${id}`}>
+          <p>
+            {artist} - {title}
+          </p>
+        </Link>
         <p>{year}</p>
       </div>
     </div>
